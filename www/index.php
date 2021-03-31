528fcb5ce0787b251a3a987b398f821dd6959e94
Yugoow <56593824+Yugoow@users.noreply.github.com>
2021-03-31 15:06:57 +0200

    add wish action

--- www/index.php
+++ www/index.php
@@ -1,19 +1,46 @@
 <?php
+namespace core;
 require_once(__DIR__.'\core\Controller.php');
+
+session_start();
+
 
 class Index extends Controller{
     protected $head;
     private Controller $parent;
+    private Wishlist $wish;
+    private Offre $offre;
 
-    function __construct($page, Controller $parent){
+    function __construct(Controller $parent){
         $this->parent = $parent;
-        $this->head = new Header($parent);
-        $this->head->tryheader($page);
+        $this->parent->head->generateHeader();
+        $this->wish = new Wishlist($this->parent);
+        $this->offre = new Offre($this->parent);
+
     }
 
+    public function generateIndex(){
+
+
+        $wlist =$this->wish->genList();
+        $wlist = $wlist[1];
+
+        $olist = $this->offre->getlastOffre();
+        $olist = $olist[1];
+
+        echo $this->parent->twig->render('index.twig', ['seq'=>$this->parent->head->menu, 'contenuoffres'=>$olist, 'contenuwish'=>$wlist]);
+    } 
+
+    function getNvlOffre(){
+        //call du model de index ?
+        //Lien entre Model et model de l'index ?
+    }
+
+
+    function getWlist(){
+        //call du model de index ?
+    }
 }
-
-
 
 $controller = new Controller();
 
@@ -24,9 +51,7 @@ if(isset($_GET['p'])){
     $page=$_GET['p'];
 }
 
-
 $controller->select($page);
-
 
 
 
